2ab7050d1d18591a70beaba8695c554e33a9031d
webstoryboy <webstupids@gmail.com>
2023-08-05 10:54:10 +0900

    😜 컴퍼넌트완료

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import "./assets/scss/style.scss";
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "./router"
 
 const app = createApp(App)
 
 app.use(router)
 
-app.mount('#app')
+app.mount("#app")
